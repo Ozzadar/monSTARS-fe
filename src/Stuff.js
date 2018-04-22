d47108bdf4ceb2a0bc95557bb99eb069fcf4b9d8
Paul Mauviel <ozzadar@ozzadar.com>
2018-04-22 15:38:03 -0400

    Copywrite banners

--- src/Stuff.js
+++ src/Stuff.js
@@ -1,3 +1,11 @@
+/*
+ * Created on Sun Apr 22 2018
+ *
+ * Copyright (c) 2018 Ozzadar.com
+ * Licensed under the GNU General Public License v3.0 
+ */
+
+
 import React from 'react';
 
 class Stuff extends React.Component {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
+/*
+ * Created on Sun Apr 22 2018
+ *
+ * Copyright (c) 2018 Ozzadar.com
+ * Licensed under the GNU General Public License v3.0 
+ */
+
+
+import React from 'react';// eslint-disable-line
 import ReactDOM from 'react-dom';
-import Main from './Main';
+import Main from './Main';// eslint-disable-line
 import "./index.css";
 
 ReactDOM.render(<Main/>, document.getElementById('root'));
